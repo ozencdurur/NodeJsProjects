1e6c5e913e5aca64d087adc807313d2ba0c24f62
Özenç Durur <ozencdurur96@gmail.com>
2023-01-13 04:40:08 +0300

    Final

---
+++ PCAT/app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController');
+
+const app = express();
+
+//connect DB
+mongoose
+  .connect(
+    'mongodb+srv://ozencdurur:Oznc.2021@ozencdurur.cfoxour.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('Database Connected!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+//ROUTES
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
+
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running port: ${port} `);
+});
