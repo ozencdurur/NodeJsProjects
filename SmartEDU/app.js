6100ea1bdb4b2f24453932a56cedba55f8150846
Özenç Durur <ozencdurur96@gmail.com>
2023-01-18 18:27:49 +0300

    SmartEDU Final

---
+++ SmartEDU/app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const methodOverride = require("method-override");
+const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
+
+const app = express();
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(
+    "mongodb+srv://ozencdurur:Oznc.2021@cluster0.t4wvea1.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("DB Connected Successfully");
+  });
+
+app.set("view engine", "ejs");
+
+global.userIN = null;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl:
+        "mongodb+srv://ozencdurur:Oznc.2021@cluster0.t4wvea1.mongodb.net/?retryWrites=true&w=majority",
+    }),
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
+
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
+app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
